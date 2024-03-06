9b8afc9b5142a82693d3e0bd8cacb35130260185
ilmari99 <i.vahteristo@gmail.com>
2024-03-06 17:09:20 +0200

    Progress. Now the rewards are correct atleast for TicTacToe

--- TicTacToe/TTTGame.py
+++ TicTacToe/TTTGame.py
@@ -29,47 +29,47 @@ class TTTGame(Game):
     def select_turn(self, players: List[TTTPlayer], previous_turns: List[int]) -> int:
         return super()._select_round_turn(players, previous_turns)
     
-    def calculate_reward(self, game_state: TTTGameState) -> float:
+    def calculate_reward(self,pid : int, game_state: TTTGameState) -> float:
         """ Calculate the reward for the player.
         If the player wins, the reward is 1.0.
         If the game is a draw, the reward is 0.5
         """
-        pid = game_state.current_player
-        #print(f"pid: {pid}")
-        #print(game_state)
-        if pid not in game_state.unfinished_players:
-            return 1
-        # If there are no -1 in the board, the game is a draw
-        if -1 not in np.array(game_state.board).flatten():
+        if self.check_player_has_won(pid, game_state):
+            return 1.0
+        # If game is a draw
+        if -1 not in np.array(game_state.board).flatten() and not any([self.check_player_has_won(p, game_state) for p in range(len(self.players)) if p != pid]):
             return 0.5
         return 0.0
     
-    def check_is_player_finished(self, pid : int, game_state: GameState) -> bool:
+    def check_player_has_won(self, pid : int, game_state: GameState) -> bool:
         """ Check if a player has a full row/col/diag
         """
         arr = np.array(game_state.board)
         # Check rows and columns
         for row_idx in range(arr.shape[0]):
             if np.all(arr[row_idx] == pid):
-                self.logger.info(f"{self.players[pid]} has won with a row")
+                self.logger.info(f"{self.players[pid]} has won with a row!")
                 return True
         for col_idx in range(arr.shape[1]):
             if np.all(arr[:, col_idx] == pid):
-                self.logger.info(f"{self.players[pid]} has won with a column")
+                self.logger.info(f"{self.players[pid]} has won with a column!")
                 return True
         if np.all(np.diag(arr) == pid) or np.all(np.diag(np.fliplr(arr)) == pid):
-            self.logger.info(f"{self.players[pid]} has won with a diagonal")
+            self.logger.info(f"{self.players[pid]} has won with a diagonal!")
             return True
-        # If we have no free spots, we have finished
-        if -1 not in arr:
-            self.logger.info(f"No available spots.")
-            return True
-        # If the other player is finished, we are finished
-        other_pid = 1 if pid == 0 else 0
-        if other_pid not in game_state.unfinished_players:
-            self.logger.info(f"Other player is finished.")
-            return True
+        return False
         
+    def check_is_player_finished(self, pid : int, game_state: GameState) -> bool:
+        """ A player is finished if the game is finished.
+        I.e. if the player has won, or if there are no more free spots, or if the other player is finished.
+        """
+        has_straight_line = self.check_player_has_won(pid, game_state)
+        if has_straight_line:
+            return True
+        if -1 not in np.array(game_state.board).flatten():
+            return True
+        if all([self.check_player_has_won(p, game_state) for p in range(len(self.players)) if p != pid]):
+            return True
         return False
     
     def get_all_possible_actions(self) -> List[TTTAction]:
