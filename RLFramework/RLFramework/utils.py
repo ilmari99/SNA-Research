95e04bca2ee16da365f7fa9df5021a17bf825e02
ilmari99 <i.vahteristo@gmail.com>
2024-03-11 18:32:22 +0200

    Added keras -> tflite converter

--- RLFramework/RLFramework/utils.py
+++ RLFramework/RLFramework/utils.py
@@ -39,6 +39,21 @@ class TFLiteModel:
         self.interpreter.invoke()
         out = self.interpreter.get_tensor(self.output_details[0]['index'])
         return list(out)
+
+def convert_model_to_tflite(file_path : str, output_file : str = None) -> None:
+    if output_file is None:
+        output_file = file_path.replace(".keras", ".tflite")
+        
+    print("Converting '{}' to '{}'".format(file_path, output_file))
+
+    model = tf.keras.models.load_model(file_path)
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+
+    with open(output_file, "wb") as f:
+        f.write(tflite_model)
+    return output_file
+        
         
 class _NoneLogger(logging.Logger):
     """ A logger that does nothing.
