a8e8701a3dae80e1dd62e179312cb32685e2823f
ilmari99 <i.vahteristo@gmail.com>
2024-03-21 19:03:40 +0200

    Changed to_vector() to take in the pid, describing whose perspective of the state should be reported

--- RLFramework/RLFramework/Result.py
+++ RLFramework/RLFramework/Result.py
@@ -38,17 +38,22 @@ class Result:
         #print(f"Number of game states: {len(self.game_states)}")
         Xs = []
         ys = []
-        for game_state in self.game_states:
-            x = game_state.to_vector()
+        total_game_states = len(self.game_states)
+        for curr_game_state_index, game_state in enumerate(self.game_states):
             # Save the state from each player's perspective.
             for perspective_pid in range(len(player_final_scores)):
-                Xs.append(x)
-                ys.append(player_final_scores[perspective_pid])
+                Xs.append(game_state.to_vector(perspective_pid))
+                ys.append(player_final_scores[perspective_pid] * (1 - self.discount_factor(game_state, curr_game_state_index + 1, total_game_states)))
         Xs = np.array(Xs, dtype=np.float16)
         ys = np.array(ys, dtype=np.float16)
         arr = np.hstack((Xs, ys.reshape(-1, 1)))
         self.save_array_to_file(arr, file_path)
         #print(f"Saved {len(Xs)} states with {Xs.shape[1]} features to {file_path}")
+        
+    def discount_factor(self, game_state, curr_game_state_num : int, total_game_states : int) -> float:
+        """ Calculate the discount factor for the current game state.
+        """
+        return 0
         
     def save_array_to_file(self, arr : np.ndarray, file_path : str) -> None:
         """ Write the array to a file.
