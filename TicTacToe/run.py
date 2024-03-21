1f85c5a33376cad9f7e327dc9b2be9e0a84de76c
ilmari99 <i.vahteristo@gmail.com>
2024-03-21 19:04:15 +0200

    Update player order and logging configuration in TicTacToe run.py

--- TicTacToe/run.py
+++ TicTacToe/run.py
@@ -7,6 +7,8 @@ from TTTPlayerMaxElems import TTTPlayerMaxElems
 from TTTResult import TTTResult
 from TTTHumanPlayer import TTTHumanPlayer
 
+import logging
+
 
 game : TTTGame = TTTGame(board_size=(3,3),
                          logger_args = {"log_level" : 10, "log_file" : "TTTGame.log"},
@@ -16,13 +18,14 @@ game : TTTGame = TTTGame(board_size=(3,3),
 )
 
 player1 = TTTPlayer("RandomPlayer1", logger_args={"log_level" : 10, "log_file" : "TTTPlayer0.log"})
-player2 = TTTPlayerNeuralNet(model_path="/home/ilmari/python/RLFramework/models/model_0.tflite",
+player2 = TTTPlayerNeuralNet(model_path="/home/ilmari/python/RLFramework/models/model_8.tflite",
                                 name=f"NeuralNetPlayer1",
                                 move_selection_temp=0,
-                                logger_args=None)
-human = TTTHumanPlayer("HumanPlayer", logger_args={"log_level" : 10, "log_file" : "TTTPlayer1.log"})
+                                logger_args={"log_level" : logging.DEBUG, "log_file" : "TTTPlayer1.log"},
+                                )
+human = TTTHumanPlayer("HumanPlayer", logger_args={"log_level" : logging.DEBUG, "log_file" : "TTTPlayer2.log"})
 
-res1 = game.play_game([human, player2])
+res1 = game.play_game([player2, human])
 
 res_json = res1.as_json(states_as_num = True)
 for k,v in res_json.items():
