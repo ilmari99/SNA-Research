8b920a76507b2e4ecf8dbcfcf9715ccd6fe4cd0e
ilmari99 <i.vahteristo@gmail.com>
2024-03-21 19:06:57 +0200

    Add discount_factor method to TTTResult class. The discount factor tells how irrelevant a position is. Typically, states further from the final reward are less relevant (discount factor =1)

--- TicTacToe/TTTResult.py
+++ TicTacToe/TTTResult.py
@@ -9,3 +9,10 @@ class TTTResult(Result):
             # Save all values as int, except the last one, which is a float.
             fmt = ["%d" for _ in range(arr.shape[1] - 1)] + ["%f"]
             np.savetxt(f, arr, delimiter=",", fmt=fmt)
+            
+    def discount_factor(self, game_state, curr_game_state_num: int, total_game_states: int) -> float:
+        """ A number between 0 and 1, representing the factor with which
+        the final score of the game state should be multiplied.
+        """
+        return 0
+        #return 1 - curr_game_state_num / total_game_states
