688d3cf5b005f7d1e1dcbae411375f8023f98891
ilmari99 <i.vahteristo@gmail.com>
2024-03-21 19:04:02 +0200

    Refactor model_fit function and add RandomRotateBoardLayer

--- TicTacToe/fit_model.py
+++ TicTacToe/fit_model.py
@@ -39,42 +39,77 @@ def players_constructor(i, model_path):
     players = [TTTPlayerNeuralNet(name=f"Player{j}_{i}",
                                     logger_args=None,
                                     model_path=np.random.choice(list(models_weighted_set.keys()), p=list(models_weighted_set.values())),
-                                    move_selection_temp=1,
+                                    move_selection_temp=0.99,
                                     )
                 for j in range(2)]
     
     return players
 
+def count_num_samples_in_ds(ds):
+    """ Count how many y values of 0, 0.5, 1 there are in the dataset.
+    """
+    num_samples = {0 : 0, 0.5 : 0, 1 : 0}
+    for x, y in ds:
+        num_samples[y.numpy()] += 1
+    return num_samples
+
+@tf.keras.saving.register_keras_serializable()
+class RandomRotateBoardLayer(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(RandomRotateBoardLayer, self).__init__(**kwargs)
+
+    def call(self, board, training=None):
+        # Randomly rotate the board only during training
+        if training:
+            board = tf.image.rot90(board, k=tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32))
+        return board
+
 def model_fit(ds, epoch, num_samples):
-    # Expand ds to have a channel dimension
-    ds = ds.map(lambda x, y: (tf.expand_dims(x, axis=-1), y), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    
     # Get the input shape from the first element of the dataset
     input_shape = ds.take(1).as_numpy_iterator().next()[0].shape
     print(f"Input shape: {input_shape}")
     print(f"Num samples: {num_samples}")
     
-    train_ds = ds.take(int(0.8*num_samples)).batch(128)
-    val_ds = ds.skip(int(0.8*num_samples)).batch(128)
+    ds = ds.shuffle(10000)
     
-    train_ds = train_ds.shuffle(10000).prefetch(tf.data.AUTOTUNE)
+    train_ds = ds.take(int(0.9*num_samples)).batch(128)
+    val_ds = ds.skip(int(0.1*num_samples)).batch(128)
+    
+    train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
 
     
     if epoch == 0:
-        input_ = tf.keras.layers.Input(shape=input_shape)
-        x = tf.keras.layers.Conv1D(32, 3, activation='relu')(input_)
-        x = tf.keras.layers.Flatten()(x)
-        x = tf.keras.layers.Dense(32, activation='relu')(x)
+        inputs = tf.keras.Input(shape=input_shape)
+        #input_len = input_shape[1]
+        
+        # Separate the input into the board and the rest
+        # Board is everything except the first 2 elements
+        board = inputs[:,2:]
+        meta = inputs[:,:2]
+        
+        meta = tf.keras.layers.Flatten()(meta)
+        
+        # Reshape the board
+        board_side_len = int(np.sqrt(board.shape[1]))
+        board = tf.keras.layers.Reshape((board_side_len, board_side_len, 1))(board)
+        board = RandomRotateBoardLayer()(board)
+        # Apply 1x2 convolution, and then 2x1 convolution
+        board = tf.keras.layers.Conv2D(16, (1,2), activation='relu')(board)
+        board = tf.keras.layers.Conv2D(32, (2,1), activation='relu')(board)
+        board = tf.keras.layers.Flatten()(board)
+        
+        # Concatenate the board and the meta
+        x = tf.keras.layers.Concatenate()([meta, board])
         x = tf.keras.layers.Dense(16, activation='relu')(x)
+        x = tf.keras.layers.Dense(8, activation='relu')(x)
+        output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
         
-        # The output is the value of the board
-        value = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-        
-        model = tf.keras.models.Model(inputs=input_, outputs=value)
+        model = tf.keras.Model(inputs=inputs, outputs=output)
 
-        
         model.compile(optimizer="adam",
                 loss='mse',
-                metrics=['mae', 'mse']
+                metrics=['mae', 'mse', "accuracy"]
         )
         print(model.summary())
     else:
@@ -82,7 +117,7 @@ def model_fit(ds, epoch, num_samples):
 
     tb_log = tf.keras.callbacks.TensorBoard(log_dir=f"logs/fit/{epoch}", histogram_freq=1)
     early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-    model.fit(train_ds, epochs=100, callbacks=[tb_log, early_stop], validation_data=val_ds)
+    model.fit(train_ds, epochs=30, callbacks=[tb_log, early_stop], validation_data=val_ds)
     model.save(f"models/model_{epoch}.keras")
     tf.keras.backend.clear_session()
     return os.path.abspath(f"models/model_{epoch}.keras")
@@ -92,11 +127,11 @@ if __name__ == "__main__":
               game_constructor,
               model_fit,
               starting_model_path="",
-              num_epochs=5,
-              num_games=2*128,
+              num_epochs=10,
+              num_games=5000,
               num_files=-1,
               num_cpus=12,
-              folder="TicTacToeModelFit",
+              folder="TicTacToeModelFitDataset",
               )
 
 
