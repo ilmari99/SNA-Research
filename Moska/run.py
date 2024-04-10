c02605777a0eb7045da050b61ff681c333483673
ilmari99 <i.vahteristo@gmail.com>
2024-04-10 15:03:26 +0300

    Refactor Moska run.py to use a loop for creating players and improve logging

--- Moska/run.py
+++ Moska/run.py
@@ -1,25 +1,28 @@
 from MoskaGame import MoskaGame
 from MoskaPlayer import MoskaPlayer
 
-players = [MoskaPlayer("Player 0",
-                       logger_args = {
-                           "log_file" : "moskaplayer0.log",
-                           "log_level" : 10,
-                           }),
-           MoskaPlayer("Player 1",
+
+for i in range(1):
+   players = [MoskaPlayer("Player 0",
                         logger_args = {
-                           "log_file" : "moskaplayer1.log",
-                           "log_level" : 10,
-                        }),
-           MoskaPlayer("Player 2",
-                        logger_args = {
-                           "log_file" : "moskaplayer2.log",
-                           "log_level" : 10,
-                        }),
-              ]
-game = MoskaGame(logger_args={"log_file" : "moskagame.log",
+                              "log_file" : "moskaplayer0.log",
                               "log_level" : 10,
-                              },
-                 gather_data = "moska_data.csv"
-)
-game.play_game(players)
+                              }),
+            MoskaPlayer("Player 1",
+                           logger_args = {
+                              "log_file" : "moskaplayer1.log",
+                              "log_level" : 10,
+                           }),
+            MoskaPlayer("Player 2",
+                           logger_args = {
+                              "log_file" : "moskaplayer2.log",
+                              "log_level" : 10,
+                           }),
+               ]
+   game = MoskaGame(logger_args={"log_file" : "moskagame.log",
+                                 "log_level" : 10,
+                                 },
+                     render_mode = "text",
+                  gather_data = "moska_data.csv"
+   )
+   game.play_game(players)
