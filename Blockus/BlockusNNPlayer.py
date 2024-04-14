d249576ebd448d18ef51e2a7c231168df0be023e
ilmari99 <i.vahteristo@gmail.com>
2024-04-14 16:14:31 +0300

    Add BlockusNNPlayer class for neural network player

---
+++ Blockus/BlockusNNPlayer.py
@@ -0,0 +1,27 @@
+from typing import List
+from BlockusGameState import BlockusGameState
+from BlockusPlayer import BlockusPlayer
+import numpy as np
+
+class BlockusNNPlayer(BlockusPlayer):
+    
+    def __init__(self,name : str = "NNPlayer",
+                 model_path : str = "",
+                 move_selection_temp = 0,
+                 logger_args : dict = None):
+        super().__init__(name=name, logger_args=logger_args)
+        assert model_path, "A model path must be given."
+        self.model_path = model_path
+        self.move_selection_temp = move_selection_temp
+        self.select_action_strategy = lambda evaluations : self._select_weighted_action(evaluations, move_selection_temp)
+        
+    def evaluate_states(self, states : List[BlockusGameState]) -> List[float]:
+        """ Evaluate the given states using the neural network.
+        """
+        # Load the model
+        model = self.game.get_model(self.model_path)
+        X = np.array([s.to_vector(self.pid) for s in states], dtype=np.float32)
+        #print(f"X shape: {X.shape}")
+        evaluations = model.predict(X)
+        #print(f"evaluations: {evaluations}")
+        return evaluations
