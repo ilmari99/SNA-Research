2db90be4e6476209abf1df5e265784d387dff3ba
ilmari99 <i.vahteristo@gmail.com>
2024-04-19 21:53:13 +0300

    Add alternate_fitting.sh script for Blockus and Moska

---
+++ Blockus/alternate_fitting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+JOB1=./Blockus/only_simulate_batch_job.sh
+JOB2=./Blockus/only_fit_batch_job.sh
+
+prev_jobid=""
+
+for i in {1..10}; do
+    jobid=$(sbatch --dependency=afterok:$prev_jobid --parsable $JOB1)
+    prev_jobid=$jobid
+    jobid=$(sbatch --dependency=afterok:$prev_jobid --parsable $JOB2)
+    prev_jobid=$jobid
+done
