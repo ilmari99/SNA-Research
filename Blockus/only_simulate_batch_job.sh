08ef0ba489f3ec89cfb080805c9a31543b5bbe2d
ilmari99 <i.vahteristo@gmail.com>
2024-04-20 14:42:52 +0300

    Refactor only_simulate_batch_job.sh scripts to remove data_folder before running

--- Blockus/only_simulate_batch_job.sh
+++ Blockus/only_simulate_batch_job.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#SBATCH --job-name=blokus_simulate
-#SBATCH --account=project_2010270
-#SBATCH --time=00:12:00
+#SBATCH --job-name=moska_simulate
+#SBATCH --account=project_2009838
+#SBATCH --time=00:15:00
 #SBATCH --partition=test
-#SBATCH --output=blokus_simulate_%j.out
-#SBATCH --error=blokus_simulate_%j.err
+#SBATCH --output=moska_simulate_%j.out
+#SBATCH --error=moska_simulate_%j.err
 #SBATCH --mail-type=END
 
 # Reserve compute
-#SBATCH --cpus-per-task=256
+#SBATCH --cpus-per-task=40
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
-#SBATCH --hint=multithread
+#SBATCH --mem-per-cpu=4G
 # Print all arguments
 echo "All arguments: $@"
 
 module purge
 module load tensorflow/2.15
 
-RLF_BLOCKUS_SCRATCH="/scratch/project_2010270/Blockus"
+RLF_MOSKA_SCRATCH="/scratch/project_2009838/Moska"
 
 PIP_EXE=./venv/bin/pip3
 PYTHON_EXE=./venv/bin/python3
@@ -45,8 +45,10 @@ $PYTHON_EXE -c "import tensorflow as tf; print(tf.__version__)"
 $PYTHON_EXE -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
 $PYTHON_EXE --version
 
-DATA_FOLDER=$RLF_BLOCKUS_SCRATCH/TestData
-MODEL_FOLDER=$RLF_BLOCKUS_SCRATCH/TestModels
+DATA_FOLDER=$RLF_MOSKA_SCRATCH/TestData
+MODEL_FOLDER=$RLF_MOSKA_SCRATCH/TestModels
+
+rm -r $DATA_FOLDER
 
 mkdir -p $DATA_FOLDER
 mkdir -p $MODEL_FOLDER
@@ -60,11 +62,11 @@ for node in $(scontrol show hostname $SLURM_JOB_NODELIST); do
         rm -r $new_data_folder
     fi
 
-    srun --nodes=1 --ntasks=1 --cpus-per-task=256 --hint=multithread -w $node $PYTHON_EXE ./Blockus/only_simulate.py \
+    srun --nodes=1 --ntasks=1 --cpus-per-task=40 -w $node $PYTHON_EXE ./Moska/only_simulate.py \
     --folder=$new_data_folder \
     --model_base_folder=$MODEL_FOLDER \
-    --num_games=896 \
-    --num_cpus=256 \
-    --num_files=256 &
+    --num_games=1050 \
+    --num_cpus=35 \
+    --num_files=350 &
 done
 wait
