248d94204d399fe461eae0fc00a7f8afb0bb3ae7
ilmari99 <i.vahteristo@gmail.com>
2024-04-20 14:44:56 +0300

    Fix conditional statement in alternate_fitting.sh script

--- Moska/alternate_fitting.sh
+++ Moska/alternate_fitting.sh
@@ -6,7 +6,11 @@ JOB2=./Moska/only_fit_batch_job.sh
 prev_jobid=""
 
 for i in {1..10}; do
-    jobid=$(sbatch --dependency=afterok:$prev_jobid --parsable $JOB1)
+    if [ "$i" -eq 1 ]; then
+    fi
+        jobid=$(sbatch --dependency=afterok:$prev_jobid --parsable $JOB1)
+    else
+        jobid=$(sbatch --parsable $JOB1)
     prev_jobid=$jobid
     jobid=$(sbatch --dependency=afterok:$prev_jobid --parsable $JOB2)
     prev_jobid=$jobid
