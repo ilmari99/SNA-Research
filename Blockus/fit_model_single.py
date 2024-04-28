816a075b3c94a2d57563b7cec8483c8054e47ff8
ilmari99 <i.vahteristo@gmail.com>
2024-04-28 22:48:11 +0300

    Improve model architecture and use the new validation data policy in fit_model_single.py

--- Blockus/fit_model_single.py
+++ Blockus/fit_model_single.py
@@ -54,18 +54,18 @@ def get_model(input_shape):
         board = tf.keras.layers.Reshape((board_side_len, board_side_len, 1))(board)
         board = RandomRotateBoardLayer()(board)
         board = RandomFlipBoardLayer()(board)
-        # Now we have the Blokus board, which is 14x14
-        # Lets apply a 3x3 convolution, and then 2x2 convolution
-        board = tf.keras.layers.Conv2D(32, (3,3), activation='relu')(board)
-        board = tf.keras.layers.Conv2D(64, (3,3), activation='relu')(board)
-        board = tf.keras.layers.Conv2D(128, (3,3), activation='relu')(board)
+        # Now we have the 20x20 board as a 3D tensor
+        # Lets apply convolutions
+        board = tf.keras.layers.Conv2D(32, (3,3), activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.01))(board)
+        board = tf.keras.layers.Conv2D(64, (3,3), activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.01))(board)
+        board = tf.keras.layers.Conv2D(128, (3,3), activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.01))(board)
         board = tf.keras.layers.Flatten()(board)
         
         # Concatenate the board and the meta
         x = tf.keras.layers.Concatenate()([meta, board])
         x = tf.keras.layers.Dense(8, activation='relu')(x)
         x = tf.keras.layers.Dense(8, activation='relu')(x)
-        output = tf.keras.layers.Dense(1, activation='relu')(x)
+        output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
         
         model = tf.keras.Model(inputs=inputs, outputs=output)
 
