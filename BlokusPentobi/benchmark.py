1d69ca564253cbc0ef3b788eae97cc1b1994724d
ilmari99 <i.vahteristo@gmail.com>
2024-07-01 11:27:34 +0300

    Added timeout

--- BlokusPentobi/benchmark.py
+++ BlokusPentobi/benchmark.py
@@ -3,6 +3,7 @@ import multiprocessing
 import os
 import random
 import argparse
+import time
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 import numpy as np
@@ -11,7 +12,7 @@ from PentobiPlayers import PentobiInternalPlayer, PentobiNNPlayer
 from utils import TFLiteModel
 import argparse
 
-def play_pentobi(i, seed, player_maker, save_data_file = "", proc_args = {}):
+def play_pentobi(i, seed, player_maker, timeout, save_data_file = "", proc_args = {}):
     
     default_proc_args = {
         "command": None,
@@ -35,18 +36,26 @@ def play_pentobi(i, seed, player_maker, save_data_file = "", proc_args = {}):
     random.seed(seed)
     
     num_moves = 0
-    while not proc.is_game_finished():
+    start_t = time.time()
+    elapsed_t = 0
+    while not proc.is_game_finished() and elapsed_t < timeout:
         pid = proc.pid
         #print(f"Player {pid} playing")
         player = players[pid-1]
         player.play_move()
         num_moves += 1
+        elapsed_t = time.time() - start_t
+    
+    if elapsed_t >= timeout:
+        print(f"Game {i} timed out")
+        proc.close()
+        return {}
+        
     if save_data_file:
         proc.write_states_to_file(save_data_file)
     score = list(proc.score)
-    pl_names = [pl.name for i,pl in enumerate(players)]
+    pl_names = [pl.name for pl in players]
     proc.close()
-    #print({pl : sc for pl,sc in zip(pl_names, score)})
     return {pl : sc for pl,sc in zip(pl_names, score)}
 
 def shuffle_players_func(players):
@@ -106,6 +115,7 @@ if __name__=="__main__":
     parser.add_argument("--model_path", type=str, required=True)
     parser.add_argument("--dont_update_win_rate", default=False, action="store_true")
     parser.add_argument("--num_internal", type=int,required=False,default=3)
+    parser.add_argument("--game_timeout", type=int, default=60)
     args = parser.parse_args()
     
     print(args)
@@ -133,7 +143,7 @@ if __name__=="__main__":
         for i in range(num_games):
             seed = np.random.randint(2**32)
             file = f""
-            yield (i, seed, _player_maker, file, kwargs)
+            yield (i, seed, _player_maker, args.game_timeout, file, kwargs)
     
     # Play the games in parallel
     results = []
--- BlokusPentobi/simulate.py
+++ BlokusPentobi/simulate.py
@@ -5,6 +5,7 @@ import multiprocessing
 import os
 import random
 import argparse
+import time
 from typing import Dict
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
@@ -14,7 +15,7 @@ from PentobiPlayers import PentobiInternalPlayer, PentobiNNPlayer#, PentobiInter
 from utils import TFLiteModel
 import argparse
 
-def play_pentobi(i, seed, player_maker, save_data_file = "", proc_args = {}):
+def play_pentobi(i, seed, player_maker, timeout, save_data_file = "", proc_args = {}):
     
     default_proc_args = {
         "command": None,
@@ -38,12 +39,21 @@ def play_pentobi(i, seed, player_maker, save_data_file = "", proc_args = {}):
     random.seed(seed)
     
     num_moves = 0
-    while not proc.is_game_finished():
+    start_t = time.time()
+    elapsed_t = 0
+    while not proc.is_game_finished() and elapsed_t < timeout:
         pid = proc.pid
         #print(f"Player {pid} playing")
         player = players[pid-1]
         player.play_move()
         num_moves += 1
+        elapsed_t = time.time() - start_t
+    
+    if elapsed_t >= timeout:
+        print(f"Game {i} timed out")
+        proc.close()
+        return {}
+        
     if save_data_file:
         proc.write_states_to_file(save_data_file)
     score = list(proc.score)
@@ -213,6 +223,7 @@ if __name__=="__main__":
     parser.add_argument("--model_folder", type=str, default=env_vars.get('model_folder', "./Models"), help="Path to model folder")
     parser.add_argument("--level", required=False, default=1,type=int)
     parser.add_argument("--model_path", type=str, required=False, default=None)
+    parser.add_argument("--game_timeout", type=int, default=60, help="Game timeout in seconds")
     args = parser.parse_args()
     
     print(args)
@@ -292,7 +303,7 @@ if __name__=="__main__":
         for i in range(num_games):
             seed = np.random.randint(2**32)
             file = f"{data_folder}/data_{i}.csv"
-            yield (i, seed, _player_maker, file, kwargs)
+            yield (i, seed, _player_maker, args.game_timeout, file, kwargs)
     
     # Play the games in parallel
     results = []
